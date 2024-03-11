93c272b7ed7bfe236db2bfe96b9018b9b6d5af10
ericksouza02 <erick_souza2104@outlook.com>
2024-03-11 20:45:36 -0300

    first commit

---
+++ desafio-frontend/src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+const Header = () => {
+    
+const Header = styled.header `
+    
+    
+`
+
+return(
+        <Header>
+            
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+        </Header>
+    )
+}
+    
+
+
+export default Header;
